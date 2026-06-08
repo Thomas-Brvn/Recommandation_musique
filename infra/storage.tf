d1f60ad3e215aeff90ea59a7966e88f93d3ade91
amarcay <marcay.alphonse@gmail.com>
2026-06-08 19:32:57 +0200

    feat: manage GCS buckets in Terraform

--- infra/storage.tf
+++ infra/storage.tf
@@ -1,60 +1,25 @@
 resource "google_storage_bucket" "tfstate" {
   name          = "tfstate-${var.project_id}"
-  location      = var.region
+  location      = "US-CENTRAL1"
   force_destroy = false
-
-  versioning {
-    enabled = true
-  }
-
-  lifecycle_rule {
-    condition {
-      num_newer_versions = 5
-    }
-    action {
-      type = "Delete"
-    }
-  }
 }
 
 resource "google_storage_bucket" "raw_listenbrainz" {
   name          = "brainz-raw-listenbrainz"
-  location      = var.region
+  location      = "EU"
   force_destroy = false
-
-  lifecycle_rule {
-    condition {
-      age = 90
-    }
-    action {
-      type = "Delete"
-    }
-  }
 }
 
 resource "google_storage_bucket" "raw_musicbrainz" {
   name          = "brainz-raw-musicbrainz"
-  location      = var.region
+  location      = "EU"
   force_destroy = false
-
-  lifecycle_rule {
-    condition {
-      age = 90
-    }
-    action {
-      type = "Delete"
-    }
-  }
 }
 
 resource "google_storage_bucket" "processed" {
   name          = "brainz-processed"
-  location      = var.region
+  location      = "EU"
   force_destroy = false
-
-  versioning {
-    enabled = true
-  }
 }
 
 resource "google_storage_bucket" "festival" {
