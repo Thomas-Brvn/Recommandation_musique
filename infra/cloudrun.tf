a71c840cefb47222db0f988dabb816b48aee18ea
amarcay <marcay.alphonse@gmail.com>
2026-06-09 10:01:27 +0200

    fix: increase Cloud Run memory to 4Gi for ALS model

--- infra/cloudrun.tf
+++ infra/cloudrun.tf
@@ -37,8 +37,8 @@ resource "google_cloud_run_v2_service" "api" {
 
       resources {
         limits = {
-          cpu    = "1"
-          memory = "2Gi"
+          cpu    = "2"
+          memory = "4Gi"
         }
         startup_cpu_boost = true
       }
